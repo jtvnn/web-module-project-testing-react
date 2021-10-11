d139f5de065cc94d1c9a22a3f678f95b57a1767f
Jason Vann <vannjt1@icloud.com>
2021-10-11 19:45:24 -0400

    episodes complete

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -8,24 +8,49 @@ const testEpisode = {
     image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
     season: 1,
     number: 1,
-    summary: "",
+    summary: "A specific summary statement",
     runtime: 1
 }
 
 const testEpisodeWithoutImage = {
     //Add in approprate test data structure here.
+    id:1,
+    name: "",
+    image: null,
+    season: 1,
+    number: 1,
+    summary: "",
+    runtime: 1
 }
 
 test("renders without error", () => {
-
+   render(<Episode episode={testEpisode}/>)
 });
 
 test("renders the summury test passed as prop", ()=>{
+    // Arrange
+    render(<Episode episode={testEpisode}/>)
+
+    // Act
+    let summaryValue = screen.queryByText(/A specific summary statement/i)
+    // Assert
+    expect(summaryValue).toBeInTheDocument()
+    expect(summaryValue).toBeTruthy()
+    expect(summaryValue).toHaveTextContent('A specific summary statement')
+   
     
 });
 
+
 test("renders default image when image is not defined", ()=>{
-    
+    // Arrange
+    render(<Episode episode={testEpisodeWithoutImage}/>)
+
+    // Act
+    let value = screen.queryByAltText('./stranger_things.png')
+    // Assert
+
+    expect(value).toBeInTheDocument()
 })
 
 //Tasks

55bafcfbfa9948a7c58cc508937167276e8b8818
Jason Vann <vannjt1@icloud.com>
2021-10-11 20:47:13 -0400

    display test complete

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,16 +1,74 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
+import Display from './../Display';
+import mockFetchShow from './../../api/fetchShow'
+jest.mock('./../../api/fetchShow')
 
+const testShow = {
+    //add in approprate test data structure here.
+    name:'test show',
+    summary: 'test summary',
+    seasons: [{
+        id: '0',
+        name: 'Season 1',
+        episodes: []
+        },
+        {
+        id: '1',
+        name: 'Season 2',
+        episodes: []
+        }
+    ],
 
+}
 
+test('renders without errors and without props', ()=>{
+    render(<Display/>)
+});
 
+test('renders Show component when the button is clicked', async () => {
+    // spy
+    mockFetchShow.mockResolvedValueOnce(testShow)
+    render(<Display/>)
+    const button = screen.getByRole('button')
+    userEvent.click(button)
 
+    const show = await screen.findByTestId('show-container')
+    expect(show).toBeInTheDocument()
+})
 
+test('renders season options matching fetch return when buttton is clicked', async ()=>{
+    // spy
+    mockFetchShow.mockResolvedValueOnce(testShow)
 
+    render(<Display/>)
+    const button = screen.getByRole('button')
+    userEvent.click(button)
 
+    await waitFor(()=> {
+        const seasonOptions = screen.queryAllByTestId('season-option')
+        expect(seasonOptions).toHaveLength(2)
+    })
+})
 
+test('displayFunc is called when the fetch button is pressed', async ()=> {
+    // spy
+    mockFetchShow.mockResolvedValueOnce(testShow)
+    // mock
+    const displayFunc = jest.fn()
 
+    render(<Display displayFunc={displayFunc}/>)
+    const button = screen.getByRole('button')
+    userEvent.click(button)
 
+await waitFor(()=>{
+    expect(displayFunc).toHaveBeenCalled()
+    
+})
 
+})
 
 
 ///Tasks:

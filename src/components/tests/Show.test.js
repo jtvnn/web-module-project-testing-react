5d854bc5c31fd7d0c7cc52c951ec7645e2953332
Jason Vann <vannjt1@icloud.com>
2021-10-11 20:22:18 -0400

    show test complete

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -6,21 +6,63 @@ import Show from './../Show';
 
 const testShow = {
     //add in approprate test data structure here.
+    name:'test show',
+    summary: 'test summary',
+    seasons: [{
+        id: '0',
+        name: 'Season 1',
+        episodes: []
+        },
+        {
+        id: '1',
+        name: 'Season 2',
+        episodes: []
+        }
+    ],
+
 }
 
 test('renders testShow and no selected Season without errors', ()=>{
+    render(<Show show={testShow} selectedSeason={'none'}/>)
 });
 
 test('renders Loading component when prop show is null', () => {
+    render(<Show show={null}/>)
+
+    let seasonValue = screen.queryByTestId('loading-container')
+
+    expect(seasonValue).toBeInTheDocument()
 });
 
 test('renders same number of options seasons are passed in', ()=>{
+    render(<Show show={testShow} selectedSeason={'none'}/>)
+
+    let value = screen.queryAllByTestId("season-option")
+
+    expect(value).toHaveLength(2)
 });
 
 test('handleSelect is called when an season is selected', () => {
+    // mock
+    const handleSelect = jest.fn()
+    render(<Show show={testShow} selectedSeason={'none'} handleSelect={handleSelect}/>)
+    const select = screen.getByLabelText(/Select A Season/i)
+    userEvent.selectOptions(select, ['1'])
+
+    expect(handleSelect).toBeCalled()
+
+
 });
 
 test('component renders when no seasons are selected and when rerenders with a season passed in', () => {
+    // rerender
+    const { rerender } = render(<Show show={testShow} selectedSeason={'none'}/>)
+    let episodes = screen.queryByTestId('episodes-container')
+    expect(episodes).not.toBeInTheDocument()
+
+    rerender(<Show show={testShow} selectedSeason={1}/>)
+    episodes = screen.queryByTestId('episodes-container')
+    expect(episodes).toBeInTheDocument()
 });
 
 //Tasks:
@@ -29,4 +71,4 @@ test('component renders when no seasons are selected and when rerenders with a s
 //3. Test that the Loading component displays when null is passed into the show prop (look at the Loading component to see how to test for it's existance)
 //4. Test that when your test data is passed through the show prop, the same number of season select options appears as there are seasons in your test data.
 //5. Test that when an item is selected, the handleSelect function is called. Look at your code to see how to get access to the select Dom element and userEvent reference materials to see how to trigger a selection.
-//6. Test that the episode component DOES NOT render when the selectedSeason props is "none" and DOES render the episode component when the selectedSeason prop has a valid season index.
+//6. Test that the episodes component DOES NOT render when the selectedSeason props is "none" and DOES render the episode component when the selectedSeason prop has a valid season index
